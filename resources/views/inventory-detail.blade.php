2ba24b65dc487e83506b76b5e5a43fa26f6cdbc7
hafeez <hafeezxpozure@gmail.com>
2020-08-31 16:20:29 +0500

    update

---
+++ resources/views/inventory-detail.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="">
+        <div class="card-body">
+            <h4 class="card-title">Personal Info</h4>
+            <div class="form-group row">
+                <label class="col-sm-3 text-right control-label col-form-label">Product Name</label>
+                <div class="col-sm-9">
+                    <div class="detail">testing</div>
+                </div>
+            </div>
+            <div class="form-group row">
+                <label class="col-sm-3 text-right control-label col-form-label">Product Name</label>
+                <div class="col-sm-9">
+                    <div class="detail">testing</div>
+                </div>
+            </div>
+            <div class="form-group row">
+                <label class="col-sm-3 text-right control-label col-form-label">Product Name</label>
+                <div class="col-sm-9">
+                    <div class="detail">testing</div>
+                </div>
+            </div>
+            <div class="form-group row">
+                <label class="col-sm-3 text-right control-label col-form-label">Product Name</label>
+                <div class="col-sm-9">
+                    <div class="detail">testing</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
